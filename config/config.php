27621b34fba93556ee09d1e86808cad1abebf174
PhamVietduc <phamvietducfpt@gmail.com>
2021-05-14 14:48:25 +0700

    add database online

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "web_mvc_asm1");
+define("DB_HOST", " sql6.freesqldatabase.com");
+define("DB_USER", "sql6412369");
+define("DB_PASS", "AgZxt48RmY");
+define("DB_NAME", "sql6412369");
 ?>
