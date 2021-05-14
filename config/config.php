19397eac65ea982afac8ebd732e61aed344947b3
PhamVietduc <phamvietducfpt@gmail.com>
2021-05-14 15:02:53 +0700

    change host

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST", "https://www.phpmyadmin.co/db_structure.php?server=1&db=sql6412369");
+define("DB_HOST", " sql6.freesqldatabase.com");
 define("DB_USER", "sql6412369");
 define("DB_PASS", "AgZxt48RmY");
 define("DB_NAME", "sql6412369");
