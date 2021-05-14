41be480d62b375a9e162bd92e33f35311d652152
PhamVietduc <phamvietducfpt@gmail.com>
2021-05-14 15:12:16 +0700

    changed homepage

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST", " sql6.freesqldatabase.com");
+define("DB_HOST", "sql6.freesqldatabase.com");
 define("DB_USER", "sql6412369");
 define("DB_PASS", "AgZxt48RmY");
 define("DB_NAME", "sql6412369");
